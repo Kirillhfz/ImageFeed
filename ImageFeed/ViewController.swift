b89535fa46c71a71cb0ec70e3f14fa3e444cd6fe
Kirill Kornakov <mtw.mindfactory@mail.ru>
2023-11-15 17:34:33 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Kirill Kornakov on 15.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
