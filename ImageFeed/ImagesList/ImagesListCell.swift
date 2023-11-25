e55eeee78b960feaded996805081fba8b9761cec
Kirill Kornakov <mtw.mindfactory@mail.ru>
2023-11-25 18:38:22 +0300

    Свёрстан экран Launch Screen. Добавлен и свёрстан по макетам экран ленты изображений.

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Kirill Kornakov on 25.11.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+}
